78a9718a209cadc07286e9f250f5ac40747b81e5
kwonssshyeon <kwonsh4396@gmail.com>
2023-11-24 16:10:35 +0900

    feat: 개발환경 세팅

---
+++ src/main/java/com/example/HappyNewHere/HappyNewHereApplication.java
@@ -0,0 +1,13 @@
+package com.example.HappyNewHere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyNewHereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyNewHereApplication.class, args);
+	}
+
+}
