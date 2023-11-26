1bc20a9917b50b10e5fd1172b450f9faa3da14ce
yooonwodyd <dbswodyd00@naver.com>
2023-11-26 23:35:46 +0900

    #5 유저 생성, 유저 검색 메서드 구현

--- src/main/java/com/example/HappyNewHere/controller/AccountController.java
+++ src/main/java/com/example/HappyNewHere/controller/AccountController.java
@@ -1,37 +1,47 @@
 package com.example.HappyNewHere.controller;
 
 
+import com.example.HappyNewHere.dto.AccountDto;
+import com.example.HappyNewHere.service.AccountService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-
 @RequiredArgsConstructor
 public class AccountController {
 
+    private final AccountService accountService;
+
+
+    // 유저 id 뿐만 아니라 발급한 토큰 또한 필요함.
     @PostMapping("/personalInfo")
     public ResponseEntity<?> personalInfo(
-            @RequestParam String nickname,
-            @RequestParam String id
+            @RequestParam String userId
     ) {
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/find/{account}")
-    public ResponseEntity<String> findUsers() {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<Page<AccountDto>> findUsers(
+            @PathVariable String account,
+            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable
+    ) {
+
+        return ResponseEntity.ok(accountService.searchAccounts(account,pageable));
     }
 
-    // createUser
-    @GetMapping("/createAccount")
-    public ResponseEntity<String> createAccount() {
-
-        return ResponseEntity.ok().build();
+    // 유저 생성 (테스트용)
+    @PostMapping("/createAccount")
+    public ResponseEntity<AccountDto> createAccount(
+            @RequestBody AccountDto accountDto
+    ) {
+        // ResponseEntity에 AccountDto를 담아서 반환
+        return ResponseEntity.ok(AccountDto.from(accountService.save(accountDto)));
     }
-
 }
