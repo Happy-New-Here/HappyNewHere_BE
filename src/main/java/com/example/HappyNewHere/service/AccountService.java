69b0ba74ea61ee56306f5fe335f0e68247e70146
yooonwodyd <dbswodyd00@naver.com>
2023-11-26 23:35:08 +0900

    #5 검색한 유저의 dto를 페이징 해서 보여주는기능 추가

--- src/main/java/com/example/HappyNewHere/service/AccountService.java
+++ src/main/java/com/example/HappyNewHere/service/AccountService.java
@@ -4,15 +4,24 @@ package com.example.HappyNewHere.service;
 import com.example.HappyNewHere.domain.Account;
 import com.example.HappyNewHere.dto.AccountDto;
 import com.example.HappyNewHere.repository.AccountRepository;
+import jakarta.transaction.Transactional;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
-@NoArgsConstructor
-public class AccoutService {
-    AccountRepository accountRepository;
+@RequiredArgsConstructor
+public class AccountService {
+    private final AccountRepository accountRepository;
 
+    @Transactional
     public Account save(AccountDto accountDto) {
         return accountRepository.save(accountDto.toEntity());
+    }
+
+    public Page<AccountDto> searchAccounts(String string, Pageable pageable) {
+        return accountRepository.findByUserIdContainingOrNicknameContaining(string, string, pageable).map(AccountDto::from);
     }
 }
