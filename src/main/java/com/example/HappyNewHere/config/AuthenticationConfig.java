f9192258dddf1dbd9d8273b60784bc28bbc5f164
yooonwodyd <dbswodyd00@naver.com>
2023-11-29 04:12:07 +0900

    로그인 로직을 처리하는 SecurityFilterChain을 빈으로 등록한다.

--- src/main/java/com/example/HappyNewHere/config/AuthenticationConfig.java
+++ src/main/java/com/example/HappyNewHere/config/AuthenticationConfig.java
@@ -1,2 +1,44 @@
-package com.example.HappyNewHere.config;public class AuthenticationConfig {
+package com.example.HappyNewHere.config;
+
+import com.example.HappyNewHere.service.AccountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class AuthenticationConfig {
+    private final AccountService accountService;
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .sessionManagement(configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize ->
+                                authorize
+                                        .requestMatchers("/login/kakao","/index.html").permitAll()
+                                        .requestMatchers("/test").hasAuthority("USER")
+//                                .requestMatchers("/user/signin","user/signup").permitAll()
+//                                .requestMatchers(HttpMethod.POST, "/**").authenticated()
+//                                .requestMatchers(HttpMethod.GET, "/**").authenticated()
+//                                .requestMatchers(HttpMethod.PUT, "/**").authenticated()
+//                                .requestMatchers(HttpMethod.DELETE, "/**").authenticated()
+                )
+                .addFilterBefore(new JwtFilterConfig(accountService,secretKey), UsernamePasswordAuthenticationFilter.class);
+
+        return httpSecurity.build();
+    }
 }
