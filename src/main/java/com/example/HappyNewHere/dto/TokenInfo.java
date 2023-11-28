76e677ac12ddef50b01bfe301410b6da9ef0dc09
yooonwodyd <dbswodyd00@naver.com>
2023-11-29 04:16:01 +0900

    #9 jwt 토큰의 정보를 담고 있는 TokenInfo를 구현

--- src/main/java/com/example/HappyNewHere/dto/TokenInfo.java
+++ src/main/java/com/example/HappyNewHere/dto/TokenInfo.java
@@ -1,2 +1,13 @@
-package com.example.HappyNewHere.dto;public class TokenInfo {
+package com.example.HappyNewHere.dto;
+
+import lombok.Data;
+
+@Data
+public class TokenInfo {
+    private String token_type;
+    private String access_token;
+    private String refresh_token;
+    private int expires_in;
+    private String scope;
+    private int refresh_token_expires_in;
 }
