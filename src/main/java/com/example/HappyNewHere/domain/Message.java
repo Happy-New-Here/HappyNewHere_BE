5744d62d16f2b156350543c4709908c5680899f5
rnjs5540 <rnjs5540@gmail.com>
2023-11-29 07:21:40 +0900

    #11 Feat: 메세지 domain, dto 작성

--- src/main/java/com/example/HappyNewHere/domain/Message.java
+++ src/main/java/com/example/HappyNewHere/domain/Message.java
@@ -1,5 +1,6 @@
 package com.example.HappyNewHere.domain;
 
+import com.example.HappyNewHere.dto.MessageDto;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -7,9 +8,11 @@ import jakarta.persistence.Id;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@NoArgsConstructor
 public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,4 +23,26 @@ public class Message {
     private String receiver;
     private int decorate;
     private boolean isHidden;
+
+    // dto -> entity
+    public Message(MessageDto messageDto) {
+        this.messageId = messageDto.getMessageId();
+        this.createdDate = messageDto.getCreatedDate();
+        this.context = messageDto.getContext();
+        this.sender = messageDto.getSender();
+        this.receiver = messageDto.getReceiver();
+        this.decorate = messageDto.getDecorate();
+        this.isHidden = messageDto.isHidden();  // 왜 바로접근가능??
+    }
+
+    // patch로 변경 ..?
+    public void update(MessageDto messageDto) {
+        this.messageId = messageDto.getMessageId();
+        this.createdDate = messageDto.getCreatedDate();
+        this.context = messageDto.getContext();
+        this.sender = messageDto.getSender();
+        this.receiver = messageDto.getReceiver();
+        this.decorate = messageDto.getDecorate();
+        this.isHidden = messageDto.isHidden();
+    }
 }
