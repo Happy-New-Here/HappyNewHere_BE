e0edb5733bcb774b14a897136a7ce4eb2a1b3270
yooonwodyd <dbswodyd00@naver.com>
2023-12-21 14:09:17 +0900

    #17 dto 에 새로운 of 메서드를 추가했습니다. from 메서드를 수정했습니다.

--- src/main/java/com/example/HappyNewHere/dto/AccountDto.java
+++ src/main/java/com/example/HappyNewHere/dto/AccountDto.java
@@ -6,18 +6,22 @@ public record AccountDto(
         Long accountId,
         String userId,
         String nickname,
-        String profileImg
+        String profileImg,
+        String stateMsg
 ) {
     public static AccountDto of(Long accountId, String userId, String nickname, String profileImg) {
-        return new AccountDto(accountId, userId, nickname, profileImg);
+        return new AccountDto(accountId, userId, nickname, profileImg, null);
+    }
+    public static AccountDto of(Long accountId, String userId, String nickname, String profileImg, String stateMsg) {
+        return new AccountDto(accountId, userId, nickname, profileImg, stateMsg);
     }
 
     public static AccountDto from(Account account) {
-        return new AccountDto(account.getAccountId(), account.getUserId(), account.getNickname(), account.getProfileImg());
+        return new AccountDto(account.getAccountId(), account.getUserId(), account.getNickname(), account.getProfileImg(), account.getStateMsg());
     }
 
     public Account toEntity() {
-        return new Account(accountId, nickname, userId, profileImg);
+        return new Account(accountId, nickname, userId, profileImg, null);
     }
 
 }
